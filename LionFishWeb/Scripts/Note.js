ca3738a66416a75b6972ac22b8d5ff8e9c889b33
jyupiter <173098D@mymail.nyp.edu.sg>
2019-01-17 23:46:12 +0800

    17/01

--- LionFishWeb/Scripts/Note.js
+++ LionFishWeb/Scripts/Note.js
@@ -4,4 +4,17 @@
         $("#newnfc").toggle();
     });
 
+    $("#newnote").on("click", function () {
+        // logic
+    });
+
+    $("#newfolder").on("click", function () {
+        // remove hardcoding
+        $("#creator").toggle();
+    });
+
+    $(".cancel").on("click", function () {
+        $("#creator").toggle();
+    });
+
 });
